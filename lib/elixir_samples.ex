94d396c30fc3c06ae87291f426e8e544ebc292c3
Diego Oliveira <oliveira.bsi@gmail.com>
2017-07-30 22:52:55 -0300

    initial commit

---
+++ lib/elixir_samples.ex
@@ -0,0 +1,18 @@
+defmodule ElixirSamples do
+  @moduledoc """
+  Documentation for ElixirSamples.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirSamples.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_samples_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirSamplesTest do
+  use ExUnit.Case
+  doctest ElixirSamples
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
